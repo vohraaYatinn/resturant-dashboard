d89b494dbfc4055474f5587c3fc6d548ff581807
Yatin Vohra <96340665+vohraaYatinn@users.noreply.github.com>
2024-10-19 20:08:41 +0530

    ok

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,2 +1,4 @@
-export const test_url = 'http://localhost:8000/';
-export const test_url_images = 'http://localhost:8000';
+// export const test_url = 'http://localhost:8000/';
+// export const test_url_images = 'http://localhost:8000';
+export const test_url = 'http://180.188.226.29:8000/';
+export const test_url_images = 'http://180.188.226.29:8000';
