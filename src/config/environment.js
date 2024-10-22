200c6341429c6d56a0b1c6a115cd9ab9ff05a4a2
Yatin Vohra <96340665+vohraaYatinn@users.noreply.github.com>
2024-10-22 17:00:06 +0530

    ok

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,4 +1,4 @@
 export const test_url = 'http://admin.osmagalhaes.pt/';
-export const test_url_images = 'http://admin.osmagalhaes.pt';
-// export const test_url = 'http://180.188.226.29:8000/';
-// export const test_url_images = 'http://180.188.226.29:8000';
+export const test_url_images = 'http://180.188.226.29/';
+// export const test_url = 'http://192.168.1.3:8000/';
+// export const test_url_images = 'http://192.168.1.3:8000';
